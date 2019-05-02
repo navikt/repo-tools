74d7b44322b3abe470f7ad30245700e3d8ef94e3
Eirik <eiriksletteberg@gmail.com>
2019-05-02 17:39:13 +0200

    try to fix script syntax

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export GITHUB_APP_PRIVATE_KEY=$(cat /opt/app/secrets/GITHUB_APP_PRIVATE_KEY)
-export OIDC_DISCOVERY_URL=$(cat /opt/app/secrets/OIDC_DISCOVERY_URL)
-export OIDC_CLIENT_ID=$(cat /opt/app/secrets/OIDC_CLIENT_ID)
-export OIDC_CLIENT_SECRET=$(cat /opt/app/secrets/OIDC_CLIENT_SECRET)
+export GITHUB_APP_PRIVATE_KEY="$(cat /opt/app/secrets/GITHUB_APP_PRIVATE_KEY)"
+export OIDC_DISCOVERY_URL="$(cat /opt/app/secrets/OIDC_DISCOVERY_URL)"
+export OIDC_CLIENT_ID="$(cat /opt/app/secrets/OIDC_CLIENT_ID)"
+export OIDC_CLIENT_SECRET="$(cat /opt/app/secrets/OIDC_CLIENT_SECRET)"
 
 /opt/app/bin/repo-tools
 
