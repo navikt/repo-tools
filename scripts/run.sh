03055a4470b5683dba34689ce60eae584c5912a8
Eirik <eiriksletteberg@gmail.com>
2019-05-03 13:50:53 +0200

    Clean up run.sh script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-
 export GITHUB_APP_PRIVATE_KEY="$(cat /opt/app/secrets/GITHUB_APP_PRIVATE_KEY)"
 export OIDC_DISCOVERY_URL="$(cat /opt/app/secrets/OIDC_DISCOVERY_URL)"
 export OIDC_CLIENT_ID="$(cat /opt/app/secrets/OIDC_CLIENT_ID)"
 export OIDC_CLIENT_SECRET="$(cat /opt/app/secrets/OIDC_CLIENT_SECRET)"
 
 /opt/app/bin/repo-tools
-
-sleep 60
