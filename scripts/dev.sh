44ba25843caf5902dc863e65e469c157b1c0818e
Eirik <eiriksletteberg@gmail.com>
2019-05-03 14:30:26 +0200

    update dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+set -e
 echo "Stack version: $(stack --numeric-version)"
 echo "Node.js version: $(node --version)"
 echo "npm version: $(npm --version)"
 echo "Docker version: $(docker --version)"
+echo "Vault CLI version: $(vault --version)"
 
-(cd frontend && npm install && npm build && npm run webpack:production)
+export VAULT_ADDR="https://vault.adeo.no"
+SECRETS=$(vault read kv/preprod/fss/repo-tools/default -format=json) ||
+    (echo "Please login to Vault, and try again: VAULT_ADDR=$VAULT_ADDR vault login -method=oidc" && exit 1)
+
+export GITHUB_APP_PRIVATE_KEY=$(echo $SECRETS | jq ".data.GITHUB_APP_PRIVATE_KEY" -r)
+export OIDC_CLIENT_ID=$(echo $SECRETS | jq ".data.OIDC_CLIENT_ID" -r)
+export OIDC_CLIENT_SECRET=$(echo $SECRETS | jq ".data.OIDC_CLIENT_SECRET" -r)
+export OIDC_DISCOVERY_URL=$(echo $SECRETS | jq ".data.OIDC_DISCOVERY_URL" -r)
+
+(cd frontend && npm ci && npm run build && npm run webpack:production)
 
 stack exec --docker-run-args="--net=bridge --publish=3000:3000 -v secrets.properties:/secrets.properties" -- yesod devel
