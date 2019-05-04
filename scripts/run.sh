761189ff5c736dd5b72c5fd72520088b02e0da75
Eirik <eiriksletteberg@gmail.com>
2019-05-04 17:39:28 +0200

    Fix error in start script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,5 @@ export GITHUB_APP_PRIVATE_KEY="$(cat /opt/app/secrets/GITHUB_APP_PRIVATE_KEY)"
 export OIDC_DISCOVERY_URL="$(cat /opt/app/secrets/OIDC_DISCOVERY_URL)"
 export OIDC_CLIENT_ID="$(cat /opt/app/secrets/OIDC_CLIENT_ID)"
 export OIDC_CLIENT_SECRET="$(cat /opt/app/secrets/OIDC_CLIENT_SECRET)"
-cat /opt/app/secrets/CLIENT_SESSION_KEY | base64 -D > config/client_session_key.aes
+cat /opt/app/secrets/CLIENT_SESSION_KEY | base64 --decode > config/client_session_key.aes
 /opt/app/bin/repo-tools
