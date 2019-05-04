6720c8328d22b0e64794447325f59dbe848a5430
Eirik <eiriksletteberg@gmail.com>
2019-05-04 18:26:49 +0200

    Fix usage of the GitHub private key

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,5 @@ export OIDC_DISCOVERY_URL="$(cat /opt/app/secrets/OIDC_DISCOVERY_URL)"
 export OIDC_CLIENT_ID="$(cat /opt/app/secrets/OIDC_CLIENT_ID)"
 export OIDC_CLIENT_SECRET="$(cat /opt/app/secrets/OIDC_CLIENT_SECRET)"
 cat /opt/app/secrets/CLIENT_SESSION_KEY | base64 --decode > config/client_session_key.aes
+cp /opt/app/secrets/GITHUB_APP_PRIVATE_KEY config/github-app-private-key.jwk
 /opt/app/bin/repo-tools
